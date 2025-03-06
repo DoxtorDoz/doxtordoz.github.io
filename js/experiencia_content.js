e1746623098f73bd28979cb769cb8b599e0dae9d
Antonio Gonzalez Capel <gonza359@gmail.com>
2025-03-06 14:03:54 +0100

    we

--- js/experiencia_content.js
+++ js/experiencia_content.js
@@ -3,7 +3,7 @@ export function getExperienciaContent() {
     return `
         <h1>Experiencia</h1>
         <span>
-            Actualmente carezco de experiencia laboral, por lo que este apartado aún queda sin información relevante.
+            Actualmente carezco de experiencia laboral, por lo que este apartado queda sin información relevante por el momento.
             <br><br>
             Si representas a una empresa, <a href="https://www.linkedin.com/in/antogoncap/">puedes ayudarme a comenzar este apartado ;)</a>
         </span>
